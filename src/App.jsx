50d3b1208d050ed23ebe1b009526f42a6d69be0c
brandonbuhr <brandonbuhr35@gmail.com>
2025-08-29 19:45:57 -0500

    basic routing structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Footer from "./components/footer";
 
-function App() {
+export default function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
-
-export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,25 @@
-function Navbar() {
-  return <>Navbar</>;
-}
+import { Link } from "react-router-dom";
 
-export default Navbar;
+export default function Navbar() {
+  return (
+    <>
+      <div className="nav-container">
+        <div className="nav-row">
+          <ul className="nav-list">
+            <li className="nav-logo">Logo</li>
+            <Link to="/about">
+              <li className="nav-item">About</li>
+            </Link>
+            <Link to="/projects">
+              <li className="nav-item">Projects</li>
+            </Link>
+            <Link to="/contact">
+              <li className="nav-item">Contact</li>
+            </Link>
+            <li className="nav-toggle">Toggle</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
