af56828d245453df1ce6188cb9477bdaa2016d4b
brandonbuhr <brandonbuhr35@gmail.com>
2025-08-30 15:31:28 -0500

    updated page structure and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import Footer from "./components/footer";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,28 @@
 import { Link } from "react-router-dom";
-
 export default function Navbar() {
   return (
     <>
       <div className="nav-container">
         <div className="nav-row">
           <ul className="nav-list">
-            <li className="nav-logo">Logo</li>
-            <Link to="/about">
-              <li className="nav-item">About</li>
-            </Link>
-            <Link to="/projects">
-              <li className="nav-item">Projects</li>
-            </Link>
-            <Link to="/contact">
-              <li className="nav-item">Contact</li>
-            </Link>
+            <li className="nav-logo">
+              <img
+                className="nav-logo-img"
+                src={"/public/BrandonBuhrLogo.png"}
+                alt="Logo"
+              />
+            </li>
+            <div className="nav-center">
+              <Link to="/about">
+                <li className="nav-item">About</li>
+              </Link>
+              <Link to="/projects">
+                <li className="nav-item">Projects</li>
+              </Link>
+              <Link to="/contact">
+                <li className="nav-item">Contact</li>
+              </Link>
+            </div>
             <li className="nav-toggle">Toggle</li>
           </ul>
         </div>
