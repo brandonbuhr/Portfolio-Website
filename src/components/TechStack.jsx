bcdec65474b7427a024bcd600a90ccac19a736e2
brandonbuhr <brandonbuhr35@gmail.com>
2025-09-03 18:41:24 -0500

    started tech stack component

---
+++ src/components/TechStack.jsx
@@ -0,0 +1,7 @@
+export default function TechStack() {
+  return (
+    <>
+      <div className="tech-container">This is my tech stack</div>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import TechStack from "../components/TechStack";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <TechStack />
       <Footer />
     </>
   );
